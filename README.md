ef382fe6d4c9093f35cbe10af899b392abd0259b
Christian Stewart <christian@paral.in>
2018-01-05 20:30:48 -0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Timestamp
+
+> Standard cross-language timestamp object.
+
+## Introduction
+
+This is a simple protobuf encoding for timestamps.
---
+++ timestamp.go
@@ -0,0 +1,36 @@
+package timestamp
+
+import (
+	"errors"
+	"time"
+)
+
+// ToTimestamp generates a millisecond timestamp from the time object.
+func ToTimestamp(t time.Time) Timestamp {
+	return Timestamp{
+		TimeUnixMs: uint64(t.UnixNano() / 1000000),
+	}
+}
+
+// ToTime generates a time object from a millisecond timestamp.
+func ToTime(t uint64) time.Time {
+	return time.Unix(0, int64(t)*1000000)
+}
+
+// ToTime converts the Timestamp to a time.Time
+func (t *Timestamp) ToTime() time.Time {
+	return ToTime(t.TimeUnixMs)
+}
+
+// Now returns a timestamp for now.
+func Now() Timestamp {
+	return ToTimestamp(time.Now())
+}
+
+// Validate checks the timestamp.
+func (t *Timestamp) Validate() error {
+	if t == nil || t.TimeUnixMs == 0 {
+		return errors.New("timestamp is empty")
+	}
+	return nil
+}
