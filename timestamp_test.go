138c942e69ca92fa2bf359a9bdce0d5249c04449
Christian Stewart <christian@paral.in>
2018-01-14 03:35:47 -0500

    tests: fix flakey test

--- timestamp_test.go
+++ timestamp_test.go
@@ -10,9 +10,11 @@ func TestNewTimestamp(t *testing.T) {
 	times := ToTimestamp(ts)
 	ta := times.ToTime()
 	var resolution = time.Millisecond * 1
-	ts = ts.Round(resolution)
-	ta = ta.Round(resolution)
-	if !ts.Equal(ta) {
+	diff := ts.Sub(ta)
+	if diff < 0 {
+		diff *= -1
+	}
+	if diff > resolution {
 		t.Logf("%s != %s", ts.String(), ta.String())
 		t.Fail()
 	}
