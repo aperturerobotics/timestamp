ffe7c2e65a624f683e8755bceec387e9653720f6
Christian Stewart <christian@paral.in>
2018-02-11 14:35:50 -0500

    fix: tests

--- test/timestamp.test.ts
+++ test/timestamp.test.ts
@@ -1,16 +1,16 @@
-import { timestamp } from './pb'
-import { newTimestamp, timestampToTime } from './timestamp-util'
+import { timestamp } from '../pb'
+import { newTimestamp, timestampToTime } from '../timestamp-util'
 
 describe('timestamp', () => {
-  it('should construct a timestamp', () => {
-    let tb = new Date()
-    let ts = newTimestamp(tb)
-    expect(ts).not.toBeNull()
-    let encTs = timestamp.Timestamp.encode(ts).finish()
-    let ats = timestamp.Timestamp.decode(encTs)
-    expect(ats).not.toBeNull()
+    it('should construct a timestamp', () => {
+        let tb = new Date()
+        let ts = newTimestamp(tb)
+        expect(ts).not.toBeNull()
+        let encTs = timestamp.Timestamp.encode(ts).finish()
+        let ats = timestamp.Timestamp.decode(encTs)
+        expect(ats).not.toBeNull()
 
-    let ta = timestampToTime(ats)
-    expect(ta).toEqual(tb)
-  })
+        let ta = timestampToTime(ats)
+        expect(ta).toEqual(tb)
+    })
 })
--- timestamp-util.ts
+++ timestamp-util.ts
@@ -2,12 +2,16 @@ import { timestamp } from './pb'
 
 // newTimestamp builds a new timestamp.
 export function newTimestamp(date?: Date): timestamp.Timestamp {
-  date = date || new Date()
-  let timeUnixMs = date.valueOf()
-  return timestamp.Timestamp.create({ timeUnixMs })
+    date = date || new Date()
+    let timeUnixMs = date.valueOf()
+    return timestamp.Timestamp.create({ timeUnixMs })
 }
 
 // timestampToTime converts a timestamp to a date.
-export function timestampToTime(timestamp: timestamp.Timestamp): Date {
-  return new Date(timestamp.timeUnixMs.toNumber())
+export function timestampToTime(timestamp: timestamp.ITimestamp | null): Date {
+    if (!timestamp || !timestamp.timeUnixMs) {
+        throw new Error('timestamp value expected')
+    }
+
+    return new Date(timestamp.timeUnixMs.toNumber())
 }
