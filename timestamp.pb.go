a7e17b2f599a0bd637c9ad5c7bb6f5ded929a034
Christian Stewart <christian@paral.in>
2022-06-09 00:45:36 -0700

    chore: update go.mod go.sum
    Signed-off-by: Christian Stewart <christian@paral.in>

--- timestamp.pb.go
+++ timestamp.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.28.0-devel
 // 	protoc        v3.19.3
-// source: github.com/aperturerobotics/bifrost/timestamp.proto
+// source: github.com/aperturerobotics/timestamp/timestamp.proto
 
 package timestamp
 
@@ -34,7 +34,7 @@ type Timestamp struct {
 func (x *Timestamp) Reset() {
 	*x = Timestamp{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_github_com_aperturerobotics_bifrost_timestamp_proto_msgTypes[0]
+		mi := &file_github_com_aperturerobotics_timestamp_timestamp_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -47,7 +47,7 @@ func (x *Timestamp) String() string {
 func (*Timestamp) ProtoMessage() {}
 
 func (x *Timestamp) ProtoReflect() protoreflect.Message {
-	mi := &file_github_com_aperturerobotics_bifrost_timestamp_proto_msgTypes[0]
+	mi := &file_github_com_aperturerobotics_timestamp_timestamp_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -60,7 +60,7 @@ func (x *Timestamp) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Timestamp.ProtoReflect.Descriptor instead.
 func (*Timestamp) Descriptor() ([]byte, []int) {
-	return file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescGZIP(), []int{0}
+	return file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Timestamp) GetTimeUnixMs() uint64 {
@@ -70,36 +70,36 @@ func (x *Timestamp) GetTimeUnixMs() uint64 {
 	return 0
 }
 
-var File_github_com_aperturerobotics_bifrost_timestamp_proto protoreflect.FileDescriptor
+var File_github_com_aperturerobotics_timestamp_timestamp_proto protoreflect.FileDescriptor
 
-var file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDesc = []byte{
-	0x0a, 0x33, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x61, 0x70, 0x65,
-	0x72, 0x74, 0x75, 0x72, 0x65, 0x72, 0x6f, 0x62, 0x6f, 0x74, 0x69, 0x63, 0x73, 0x2f, 0x62, 0x69,
-	0x66, 0x72, 0x6f, 0x73, 0x74, 0x2f, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x2e,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70,
-	0x22, 0x2d, 0x0a, 0x09, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x12, 0x20, 0x0a,
-	0x0c, 0x74, 0x69, 0x6d, 0x65, 0x5f, 0x75, 0x6e, 0x69, 0x78, 0x5f, 0x6d, 0x73, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x04, 0x52, 0x0a, 0x74, 0x69, 0x6d, 0x65, 0x55, 0x6e, 0x69, 0x78, 0x4d, 0x73, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDesc = []byte{
+	0x0a, 0x35, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x61, 0x70, 0x65,
+	0x72, 0x74, 0x75, 0x72, 0x65, 0x72, 0x6f, 0x62, 0x6f, 0x74, 0x69, 0x63, 0x73, 0x2f, 0x74, 0x69,
+	0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x2f, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d,
+	0x70, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61,
+	0x6d, 0x70, 0x22, 0x2d, 0x0a, 0x09, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x12,
+	0x20, 0x0a, 0x0c, 0x74, 0x69, 0x6d, 0x65, 0x5f, 0x75, 0x6e, 0x69, 0x78, 0x5f, 0x6d, 0x73, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x0a, 0x74, 0x69, 0x6d, 0x65, 0x55, 0x6e, 0x69, 0x78, 0x4d,
+	0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescOnce sync.Once
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescData = file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDesc
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescOnce sync.Once
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescData = file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDesc
 )
 
-func file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescGZIP() []byte {
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescOnce.Do(func() {
-		file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescData = protoimpl.X.CompressGZIP(file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescData)
+func file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescGZIP() []byte {
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescOnce.Do(func() {
+		file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescData = protoimpl.X.CompressGZIP(file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescData)
 	})
-	return file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDescData
+	return file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDescData
 }
 
-var file_github_com_aperturerobotics_bifrost_timestamp_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_github_com_aperturerobotics_bifrost_timestamp_proto_goTypes = []interface{}{
+var file_github_com_aperturerobotics_timestamp_timestamp_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_github_com_aperturerobotics_timestamp_timestamp_proto_goTypes = []interface{}{
 	(*Timestamp)(nil), // 0: timestamp.Timestamp
 }
-var file_github_com_aperturerobotics_bifrost_timestamp_proto_depIdxs = []int32{
+var file_github_com_aperturerobotics_timestamp_timestamp_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -107,13 +107,13 @@ var file_github_com_aperturerobotics_bifrost_timestamp_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_github_com_aperturerobotics_bifrost_timestamp_proto_init() }
-func file_github_com_aperturerobotics_bifrost_timestamp_proto_init() {
-	if File_github_com_aperturerobotics_bifrost_timestamp_proto != nil {
+func init() { file_github_com_aperturerobotics_timestamp_timestamp_proto_init() }
+func file_github_com_aperturerobotics_timestamp_timestamp_proto_init() {
+	if File_github_com_aperturerobotics_timestamp_timestamp_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_github_com_aperturerobotics_bifrost_timestamp_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_github_com_aperturerobotics_timestamp_timestamp_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Timestamp); i {
 			case 0:
 				return &v.state
@@ -130,18 +130,18 @@ func file_github_com_aperturerobotics_bifrost_timestamp_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDesc,
+			RawDescriptor: file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_github_com_aperturerobotics_bifrost_timestamp_proto_goTypes,
-		DependencyIndexes: file_github_com_aperturerobotics_bifrost_timestamp_proto_depIdxs,
-		MessageInfos:      file_github_com_aperturerobotics_bifrost_timestamp_proto_msgTypes,
+		GoTypes:           file_github_com_aperturerobotics_timestamp_timestamp_proto_goTypes,
+		DependencyIndexes: file_github_com_aperturerobotics_timestamp_timestamp_proto_depIdxs,
+		MessageInfos:      file_github_com_aperturerobotics_timestamp_timestamp_proto_msgTypes,
 	}.Build()
-	File_github_com_aperturerobotics_bifrost_timestamp_proto = out.File
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_rawDesc = nil
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_goTypes = nil
-	file_github_com_aperturerobotics_bifrost_timestamp_proto_depIdxs = nil
+	File_github_com_aperturerobotics_timestamp_timestamp_proto = out.File
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_rawDesc = nil
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_goTypes = nil
+	file_github_com_aperturerobotics_timestamp_timestamp_proto_depIdxs = nil
 }
