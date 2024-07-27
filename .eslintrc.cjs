009a37967509a77e51522be5139237b9ae2e10cb
Christian Stewart <christian@aperture.us>
2024-07-27 02:36:39 -0700

    refactor: remove unused react hooks linter
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'prettier',
   ],
   parserOptions: {
